a13db4b846f9529fd72bec4fc93cdbc0e4e82267
quadrel-design <147716606+quadrel-design@users.noreply.github.com>
2025-04-06 22:49:06 +0200

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,25 +1,36 @@
-import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
-import { Head } from "./components/Head";
-import { useNextSeoProps } from "./config/useNextSeoProps";
-import { Logo } from "./components/Logo";
+import type { DocsThemeConfig } from "nextra-theme-docs";
+import Head from "next/head";
+import { Logo } from "./components/logo";
+import { useNextSeoProps } from "./components/seo";
 
 const config: DocsThemeConfig = {
   logo: <Logo />,
+
   project: {
     link: "https://github.com/mkrtchian/reading-notes",
   },
+
   docsRepositoryBase: "https://github.com/mkrtchian/reading-notes/blob/main",
+
   footer: {
     text: "Made by Roman Mkrtchian",
   },
+
   head: Head,
+
   feedback: {
     content: () => <>Question? Give me feedback →</>,
     labels: "feedback",
   },
+
   useNextSeoProps,
+
   i18n: [],
+
+  // ✅ Suchfunktion aktivieren:
+  search: {
+    placeholder: "Search...",
+  },
 };
 
 export default config;
